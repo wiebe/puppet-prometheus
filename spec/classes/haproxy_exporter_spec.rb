1c39be9de1c94fc40a2b95b85bc2678b4639f187
Jon Ward <jghward@gmail.com>
2017-01-31 17:19:12 +0000

    Add a class for the HAProxy exporter (#10)

---
+++ spec/classes/haproxy_exporter_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'prometheus::haproxy_exporter' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge(
+          staging_http_get: 'curl'
+        )
+      end
+
+      context 'with version specified' do
+        let(:params) do
+          {
+            version: '0.7.1',
+            arch: 'amd64',
+            os: 'linux'
+          }
+        end
+        describe 'install correct binary' do
+          it { is_expected.to contain_file('/usr/local/bin/haproxy_exporter').with('target' => '/opt/staging/haproxy_exporter-0.7.1.linux-amd64/haproxy_exporter') }
+        end
+      end
+    end
+  end
+end
