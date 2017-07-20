86f298c9a09252165c6d3dc0e7fd2d6e0291c049
Stas Alekseev <stas.alekseev@gmail.com>
2017-07-19 21:07:11 -0400

    Added unit test for mongodb_exporter

---
+++ spec/classes/mongodb_exporter_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'prometheus::mongodb_exporter' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      context 'with version specified' do
+        let(:params) do
+          {
+            version: '0.3.0',
+            arch: 'amd64',
+            os: 'linux'
+          }
+        end
+
+        describe 'install correct binary' do
+          it { is_expected.to contain_file('/usr/local/bin/mongodb_exporter').with('target' => '/opt/mongodb_exporter-0.3.0.linux-amd64/mongodb_exporter') }
+        end
+      end
+    end
+  end
+end
