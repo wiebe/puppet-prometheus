333b70137c65da8a65674385531ebf48b49c9b48
Tim Meusel <tim@bastelfreak.de>
2017-10-24 01:14:28 +0200

    cleanup spec_helper_acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,10 +2,9 @@ require 'beaker-rspec'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
-run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
-install_module_on(hosts)
-install_module_dependencies_on(hosts)
+run_puppet_install_helper
+install_module
+install_module_dependencies
 
 RSpec.configure do |c|
   # Readable test descriptions
