6204a845602394e2be5fff1478046d7a771b1281
Martin Alfke <ma@example42.com>
2017-11-20 16:03:57 +0100

    convert Facter output to yaml

--- lib/facter/alert_manager_running.rb
+++ lib/facter/alert_manager_running.rb
@@ -1,6 +1,7 @@
+require 'YAML'
 Facter.add('prometheus_alert_manager_running') do
   setcode do
-    svc_status = Facter::Core::Execution.exec('puppet resource service alert_manager --to_yaml')
+    svc_status = YAML.load(Facter::Core::Execution.exec('puppet resource service alert_manager --to_yaml'))
     svc_status['service']['alert_manager']['ensure']
   end
 end
