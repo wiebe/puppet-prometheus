87ee6d7723f6095475bd1f8dd787f2253bcdbf1c
Martin Alfke <ma@example42.com>
2017-11-24 13:40:37 +0100

    older versions of puppet don't know about the --to_yaml option
    switching back to scanning the result
    
    fixes #118

--- lib/facter/alert_manager_running.rb
+++ lib/facter/alert_manager_running.rb
@@ -1,6 +1,5 @@
 Facter.add('prometheus_alert_manager_running') do
   setcode do
-    svc_status = YAML.load(Facter::Core::Execution.exec('puppet resource service alert_manager --to_yaml'))
-    svc_status['service']['alert_manager']['ensure']
+    Facter::Core::Execution.exec('puppet resource service alert_manager').scan('running')[0]
   end
 end
