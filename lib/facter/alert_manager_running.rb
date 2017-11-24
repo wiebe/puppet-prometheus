f0b77e30902dd366006b812135ab9b4969dd2037
Martin Alfke <ma@example42.com>
2017-11-24 13:51:16 +0100

    add error handling

--- lib/facter/alert_manager_running.rb
+++ lib/facter/alert_manager_running.rb
@@ -1,5 +1,9 @@
 Facter.add('prometheus_alert_manager_running') do
   setcode do
-    Facter::Core::Execution.exec('puppet resource service alert_manager').scan('running')[0]
+    begin
+      Facter::Core::Execution.execute('puppet resource service alert_manager').scan('running')[0]
+    rescue Facter::Core::Execution::ExecutionFailure
+      'not installed'
+    end
   end
 end
