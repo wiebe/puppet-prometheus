35dc9ccf5d3f278a228dc5aecefd32a8aca9c4a3
Stefan - Zipkid - Goethals <stefan.goethals@vrt.be>
2018-02-23 14:41:15 +0100

    Make prometheus::alerts a define so we can reuse it

--- manifests/alerts.pp
+++ manifests/alerts.pp
@@ -11,29 +11,32 @@
 #  [*alertfile_name*]
 #  Filename to use when storing the alerts file
 #
-class prometheus::alerts (
-  String $location,
+define prometheus::alerts (
   Variant[Array,Hash] $alerts,
-  String $alertfile_name = $prometheus::params::alertfile_name,
-) inherits prometheus::params {
+  String $location = "$::prometheus::config_dir/rules",
+) {
   if ( versioncmp($::prometheus::version, '2.0.0') < 0 ){
-    file { "${location}/${alertfile_name}":
+    file { "${location}/${name}.rules":
       ensure       => 'file',
       owner        => $prometheus::user,
       group        => $prometheus::group,
       notify       => Class['::prometheus::service_reload'],
       content      => epp("${module_name}/alerts.epp"),
       validate_cmd => "${prometheus::bin_dir}/promtool check-rules %",
+      require      => Class['::prometheus::install'],
+      before       => Class['::prometheus::config'],
     }
   }
   else {
-    file { "${location}/${alertfile_name}":
+    file { "${location}/${name}.rules":
       ensure       => 'file',
       owner        => $prometheus::user,
       group        => $prometheus::group,
       notify       => Class['::prometheus::service_reload'],
       content      => $alerts.to_yaml,
       validate_cmd => "${prometheus::bin_dir}/promtool check rules %",
+      require      => Class['::prometheus::install'],
+      before       => Class['::prometheus::config'],
     }
   }
 }
