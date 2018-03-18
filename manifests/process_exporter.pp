d281533957ed90646090addd24c1352794fedb25
Tim Meusel <tim@bastelfreak.de>
2018-03-18 14:46:15 +0100

    process_exporter

--- manifests/process_exporter.pp
+++ manifests/process_exporter.pp
@@ -79,7 +79,7 @@ class prometheus::process_exporter(
   String $user,
   String $version,
   Stdlib::Absolutepath $config_path,
-  Array $watched_processes                                           = []
+  Array $watched_processes                                           = [],
   Boolean $purge_config_dir                                          = true,
   Boolean $restart_on_change                                         = true,
   Boolean $service_enable                                            = true,
