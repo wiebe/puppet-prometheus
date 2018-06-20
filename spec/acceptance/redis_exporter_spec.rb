76973b846bfe4723721660bfe3217a92d46371e4
Niels Jansen <n.jansen@tmg.nl>
2018-06-20 15:11:24 +0200

    add spec test for redis_export

---
+++ spec/acceptance/redis_exporter_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper_acceptance'
+
+describe 'prometheus redis exporter' do
+  it 'redis_exporter works idempotently with no errors' do
+    pp = 'include prometheus::redis_exporter'
+    # Run it twice and test for idempotency
+    apply_manifest(pp, catch_failures: true)
+    apply_manifest(pp, catch_changes: true)
+  end
+
+  describe service('redis_exporter') do
+    it { is_expected.to be_running }
+    it { is_expected.to be_enabled }
+  end
+  # the class installs an the redis_exporter that listens on port 9121
+  # it should not install the prometheus server (port 9090)
+  describe port(9121) do
+    it { is_expected.to be_listening.with('tcp6') }
+  end
+  describe port(9090) do
+    it { is_expected.not_to be_listening.with('tcp6') }
+  end
+end
