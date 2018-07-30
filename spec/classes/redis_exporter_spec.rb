1521bab93c699f3d316ced4f59f7d190e8729215
Niels Jansen <n.jansen@tmg.nl>
2018-07-30 12:58:10 +0200

    created test with wrong service name

--- spec/classes/redis_exporter_spec.rb
+++ spec/classes/redis_exporter_spec.rb
@@ -27,7 +27,7 @@ describe 'prometheus::redis_exporter' do
         describe 'required resources' do
           it { is_expected.to contain_group('redis-exporter') }
           it { is_expected.to contain_prometheus__daemon('redis_exporter') }
-          it { is_expected.to contain_service('prometheus_redis_exporter') }
+          it { is_expected.to contain_service('redis_exporter') }
           it { is_expected.to contain_user('redis-exporter') }
         end
       end
