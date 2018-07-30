4b9cf9a4ce213a67931bc18edb38f4ab1d119416
Niels Jansen <github@nielsj.net>
2018-07-30 22:33:53 +0200

    add postgres exporter (#236)
    * add postgres exporter
    
    * spec fixes
    
    * spec fixes
    
    * default to no env vars, and extra input checks
    
    * add postgres exporter
    
    * add pointer to postgres_exporter module
    
    * stricter data typing, spectest enhanchements
    
    * tuning spec tests
    
    * change spec tests
    
    * fix rubocop.
    
    * update to follow suggestions of review
    
    * add a comma, spec fixing..
    
    * set required params in acceptance test.

---
+++ spec/acceptance/postgres_exporter_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper_acceptance'
+
+describe 'prometheus postgres exporter' do
+  it 'postgres_exporter works idempotently with no errors' do
+    pp = "class{'prometheus::postgres_exporter': postgres_pass => 'password', postgres_user => 'username' }"
+    # Run it twice and test for idempotency
+    apply_manifest(pp, catch_failures: true)
+    apply_manifest(pp, catch_changes: true)
+  end
+
+  describe service('postgres_exporter') do
+    it { is_expected.to be_running }
+    it { is_expected.to be_enabled }
+  end
+  # the class installs an the postgres_exporter that listens on port 9187
+  describe port(9187) do
+    it { is_expected.to be_listening.with('tcp6') }
+  end
+end
