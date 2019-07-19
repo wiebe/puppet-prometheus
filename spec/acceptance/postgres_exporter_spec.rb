8bbb1dc6502c6fdf07625884665ce5a876ce20b0
Tim Meusel <tim@bastelfreak.de>
2019-07-19 13:16:14 +0200

    postgres_exporter: update 0.4.6->0.5.1

--- spec/acceptance/postgres_exporter_spec.rb
+++ spec/acceptance/postgres_exporter_spec.rb
@@ -30,4 +30,36 @@ describe 'prometheus postgres exporter' do
       it { is_expected.to be_enabled }
     end
   end
+  describe 'postgres_exporter update from 0.4.6 to 0.5.1' do
+    it 'is idempotent' do
+      pp = "class{'prometheus::postgres_exporter': postgres_auth_method => 'custom', group => 'postgres', user => 'postgres', data_source_custom => {'DATA_SOURCE_NAME' => 'user=postgres host=/var/run/postgresql/ sslmode=disable',}, version => '0.4.6'}"
+      # Run it twice and test for idempotency
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
+    end
+
+    describe service('postgres_exporter') do
+      it { is_expected.to be_running }
+      it { is_expected.to be_enabled }
+    end
+
+    describe port(9187) do
+      it { is_expected.to be_listening.with('tcp6') }
+    end
+    it 'is idempotent' do
+      pp = "class{'prometheus::postgres_exporter': postgres_auth_method => 'custom', group => 'postgres', user => 'postgres', data_source_custom => {'DATA_SOURCE_NAME' => 'user=postgres host=/var/run/postgresql/ sslmode=disable',}, version => '0.5.1'}"
+      # Run it twice and test for idempotency
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
+    end
+
+    describe service('postgres_exporter') do
+      it { is_expected.to be_running }
+      it { is_expected.to be_enabled }
+    end
+
+    describe port(9187) do
+      it { is_expected.to be_listening.with('tcp6') }
+    end
+  end
 end
