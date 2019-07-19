01bc189dff948c4a5b80fc096a50dd40be5a4096
Tim Meusel <tim@bastelfreak.de>
2019-07-19 12:35:22 +0200

    statsd: update 0.8.0->0.12.1

--- spec/acceptance/statsd_exporter_spec.rb
+++ spec/acceptance/statsd_exporter_spec.rb
@@ -3,20 +3,27 @@ require 'spec_helper_acceptance'
 describe 'prometheus statsd exporter' do
   it 'statsd_exporter works idempotently with no errors' do
     pp = 'include prometheus::statsd_exporter'
-    # Run it twice and test for idempotency
     apply_manifest(pp, catch_failures: true)
     apply_manifest(pp, catch_changes: true)
   end
-
-  describe 'prometheus statsd exporter version 0.5.0' do
-    it ' statsd_exporter installs with version 0.5.0' do
-      pp = "class {'prometheus::statsd_exporter': version => '0.5.0' }"
-      # Run it twice and test for idempotency
+  describe service('statsd_exporter') do
+    it { is_expected.to be_running }
+    it { is_expected.to be_enabled }
+  end
+  describe port(9102) do
+    it { is_expected.to be_listening.with('tcp6') }
+  end
+  describe port(9125) do
+    it { is_expected.to be_listening.with('udp6') }
+  end
+  describe 'prometheus statsd exporter version 0.8.0' do
+    it ' statsd_exporter installs with version 0.8.0' do
+      pp = "class {'prometheus::statsd_exporter': version => '0.8.0' }"
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
     end
     describe process('statsd_exporter') do
-      its(:args) { is_expected.to match %r{\ -statsd.mapping-config} }
+      its(:args) { is_expected.to match %r{\ --statsd.mapping-config} }
     end
     describe service('statsd_exporter') do
       it { is_expected.to be_running }
@@ -30,9 +37,9 @@ describe 'prometheus statsd exporter' do
     end
   end
 
-  describe 'prometheus statsd exporter version 0.7.0' do
-    it ' statsd_exporter installs with version 0.7.0' do
-      pp = "class {'prometheus::statsd_exporter': version => '0.7.0' }"
+  describe 'prometheus statsd exporter version 0.12.1' do
+    it ' statsd_exporter installs with version 0.12.1' do
+      pp = "class {'prometheus::statsd_exporter': version => '0.12.1' }"
       # Run it twice and test for idempotency
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
