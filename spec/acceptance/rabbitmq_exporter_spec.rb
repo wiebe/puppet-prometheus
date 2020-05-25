46a269ae54c9522942c06315838e89af8fdd46a3
Dennis Hoppe <github@debian-solutions.de>
2020-05-25 14:30:31 +0200

    Change hierarchy for acceptance tests

--- spec/acceptance/rabbitmq_exporter_spec.rb
+++ spec/acceptance/rabbitmq_exporter_spec.rb
@@ -10,22 +10,12 @@ describe 'prometheus rabbitmq_exporter' do
         scrape_port    => 9419,
       }
     EOS
-    # Run it twice and test for idempotency
     apply_manifest(pp, catch_failures: true)
     apply_manifest(pp, catch_changes: true)
   end
 
-  describe service('rabbitmq_exporter') do
-    it { is_expected.to be_running }
-    it { is_expected.to be_enabled }
-  end
-
-  describe port(9090) do
-    it { is_expected.to be_listening.with('tcp6') }
-  end
-
-  describe 'rabbitmq_exporter update from 0.25.2 to 0.29.0' do
-    it 'is idempotent' do
+  describe 'prometheus rabbitmq_exporter version 0.25.2' do
+    it 'rabbitmq_exporter installs with version 0.25.2' do
       pp = <<-EOS
         class { 'prometheus::rabbitmq_exporter':
           version        => '0.25.2',
@@ -35,7 +25,6 @@ describe 'prometheus rabbitmq_exporter' do
           scrape_port    => 9419,
         }
       EOS
-      # Run it twice and test for idempotency
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
     end
@@ -48,7 +37,10 @@ describe 'prometheus rabbitmq_exporter' do
     describe port(9090) do
       it { is_expected.to be_listening.with('tcp6') }
     end
-    it 'is idempotent' do
+  end
+
+  describe 'prometheus rabbitmq_exporter version 0.29.0' do
+    it 'rabbitmq_exporter installs with version 0.29.0' do
       pp = <<-EOS
         class { 'prometheus::rabbitmq_exporter':
           version        => '0.29.0',
@@ -58,7 +50,6 @@ describe 'prometheus rabbitmq_exporter' do
           scrape_port    => 9419,
         }
       EOS
-      # Run it twice and test for idempotency
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
     end
