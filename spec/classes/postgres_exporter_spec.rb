a90b3462ce00a410a535cfa0bc3786fd975ce4db
Tim Meusel <tim@bastelfreak.de>
2020-06-20 16:12:35 +0200

    Postgres Exporter: Update 0.5.1->0.8.0

--- spec/classes/postgres_exporter_spec.rb
+++ spec/classes/postgres_exporter_spec.rb
@@ -23,10 +23,25 @@ describe 'prometheus::postgres_exporter' do
         describe 'with all defaults' do
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_file('/usr/local/bin/postgres_exporter').with('target' => '/opt/postgres_exporter-0.4.6.linux-amd64/postgres_exporter') }
+          it { is_expected.to contain_class('prometheus') }
           it { is_expected.to contain_prometheus__daemon('postgres_exporter') }
           it { is_expected.to contain_user('postgres-exporter') }
           it { is_expected.to contain_group('postgres-exporter') }
           it { is_expected.to contain_service('postgres_exporter') }
+          it { is_expected.to contain_archive('/tmp/postgres_exporter-0.4.6.tar.gz') }
+          it { is_expected.to contain_file('/opt/postgres_exporter-0.4.6.linux-amd64') }
+
+          if facts[:os]['release']['major'].to_i == 6
+            it { is_expected.to contain_file('/etc/init.d/postgres_exporter') }
+          else
+            it { is_expected.to contain_systemd__unit_file('postgres_exporter.service') }
+          end
+
+          if facts[:os]['family'] == 'RedHat'
+            it { is_expected.to contain_file('/etc/sysconfig/postgres_exporter') }
+          else
+            it { is_expected.to contain_file('/etc/default/postgres_exporter') }
+          end
         end
       end
     end
