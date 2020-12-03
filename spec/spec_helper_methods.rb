5975ff32e5a08284e63708860895cab3a0ef51b2
Tim Meusel <tim@bastelfreak.de>
2020-12-03 10:03:59 +0100

    simplify spec_helper_methods

--- spec/spec_helper_methods.rb
+++ spec/spec_helper_methods.rb
@@ -1,19 +1,5 @@
 def os_specific_facts(facts)
-  case facts[:os]['family']
-  when 'Archlinux'
-    { service_provider: 'systemd' }
-  when 'Debian'
-    case facts[:os]['release']['major']
-    when '9'
-      { service_provider: 'systemd' }
-    when '10'
-      { service_provider: 'systemd' }
-    when '16.04'
-      { service_provider: 'systemd' }
-    when '18.04'
-      { service_provider: 'systemd' }
-    end
-  when 'RedHat'
+  if ['Archlinux', 'Debian', 'RedHat'].include?(facts[:os]['family'])
     { service_provider: 'systemd' }
   end
 end
