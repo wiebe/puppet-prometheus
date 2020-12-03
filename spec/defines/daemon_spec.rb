ecab404473d535ca034ef63af7934ae9f27b3af0
Tim Meusel <tim@bastelfreak.de>
2020-12-03 23:12:38 +0100

    cleanup unit test files

--- spec/defines/daemon_spec.rb
+++ spec/defines/daemon_spec.rb
@@ -91,76 +91,31 @@ describe 'prometheus::daemon' do
             it { is_expected.to contain_archive("/tmp/smurf_exporter-#{parameters[:version]}.tar.gz").with_extract_path('/opt/foo') }
           end
 
-          if ['centos-7-x86_64', 'centos-8-x86_64', 'debian-9-x86_64', 'debian-10-x86_64', 'redhat-7-x86_64', 'redhat-8-x86_64', 'ubuntu-16.04-x86_64', 'ubuntu-18.04-x86_64',  'ubuntu-20.04-x86_64', 'archlinux-5-x86_64', 'virtuozzolinux-7-x86_64'].include?(os)
-            # init_style = 'systemd'
+          it { is_expected.to contain_class('systemd') }
 
-            it { is_expected.to contain_class('systemd') }
+          it {
+            is_expected.to contain_systemd__unit_file('smurf_exporter.service').that_notifies(
+              'Service[smurf_exporter]'
+            ).with_content(
+              %r{User=smurf_user\n}
+            ).with_content(
+              %r{ExecStart=/usr/local/bin/smurf_exporter\n\nExecReload=}
+            )
+          }
 
-            it {
-              is_expected.to contain_systemd__unit_file('smurf_exporter.service').that_notifies(
-                'Service[smurf_exporter]'
-              ).with_content(
-                %r{User=smurf_user\n}
-              ).with_content(
-                %r{ExecStart=/usr/local/bin/smurf_exporter\n\nExecReload=}
-              )
-            }
+          if Gem::Version.new(facts[:puppetversion]) < Gem::Version.new('6.1.0')
+            it { is_expected.to contain_class('systemd::systemctl::daemon_reload').that_comes_before('Service[smurf_exporter]') }
+          end
 
-            if Gem::Version.new(facts[:puppetversion]) < Gem::Version.new('6.1.0')
-              it { is_expected.to contain_class('systemd::systemctl::daemon_reload').that_comes_before('Service[smurf_exporter]') }
+          context 'with overidden bin_name' do
+            let(:params) do
+              super().merge(bin_name: 'notsmurf_exporter')
             end
 
-            context 'with overidden bin_name' do
-              let(:params) do
-                parameters.merge(bin_name: 'notsmurf_exporter')
-              end
-
-              it {
-                is_expected.to contain_systemd__unit_file('smurf_exporter.service').with_content(
-                  %r{ExecStart=/usr/local/bin/notsmurf_exporter}
-                )
-              }
-            end
-
-          elsif ['ubuntu-14.04-x86_64'].include?(os)
-            # init_style = 'upstart'
-
             it {
-              is_expected.to contain_file('/etc/init/smurf_exporter.conf').with(
-                'mode'    => '0444',
-                'owner'   => 'root',
-                'group'   => 'root'
-              ).with_content(
-                %r{env USER=smurf_user\n}
-              ).with_content(
-                %r{exec start-stop-daemon -c \$USER -g \$GROUP -p \$PID_FILE -x \$DAEMON -S -- \n\nend script}
+              is_expected.to contain_systemd__unit_file('smurf_exporter.service').with_content(
+                %r{ExecStart=/usr/local/bin/notsmurf_exporter}
               )
-            }
-
-            it {
-              is_expected.to contain_file('/etc/init.d/smurf_exporter').with(
-                'ensure' => 'link',
-                'target' => '/lib/init/upstart-job',
-                'owner'  => 'root',
-                'group'  => 'root',
-                'mode'   => '0755'
-              )
-            }
-
-            context 'with overidden bin_name' do
-              let(:params) do
-                parameters.merge(bin_name: 'notsmurf_exporter')
-              end
-
-              it {
-                is_expected.to contain_file('/etc/init/smurf_exporter.conf').with_content(
-                  %r{env DAEMON=/usr/local/bin/notsmurf_exporter}
-                )
-              }
-            end
-          else
-            it {
-              is_expected.to raise_error(Puppet::Error, %r{I don't know how to create an init script for style})
             }
           end
 
@@ -174,7 +129,7 @@ describe 'prometheus::daemon' do
                 %r{SOMEVAR="42"\n}
               )
             }
-          elsif ['centos-6-x86_64', 'redhat-6-x86_64', 'centos-7-x86_64', 'redhat-7-x86_64'].include?(os)
+          elsif ['centos-7-x86_64', 'redhat-7-x86_64'].include?(os)
             it {
               is_expected.to contain_file('/etc/sysconfig/smurf_exporter').with(
                 'mode'    => '0644',
@@ -193,6 +148,7 @@ describe 'prometheus::daemon' do
               'enable' => true
             )
           }
+
           context 'exported resources' do
             subject { exported_resources }
 
@@ -206,6 +162,7 @@ describe 'prometheus::daemon' do
           end
         end
       end
+
       context 'with scrape_job_labels specified' do
         let(:params) do
           {
